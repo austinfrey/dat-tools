bd621e98747b64527b85cd54b6afc9240db2bf7f
austinfrey <aafrey85@gmail.com>
2018-08-09 09:37:09 -0400

    begin implementing tests in all packages

---
+++ packages/dat-choo-websocket/tests/test.js
@@ -0,0 +1,6 @@
+const test = require('tape')
+
+test('First test.', function (t) {
+	t.plan(1)
+	t.ok(true)
+})
---
+++ packages/dat-socket/tests/test.js
@@ -0,0 +1,7 @@
+const test = require('tape')
+const datSocket = require('..')
+
+test('First test.', function (t) {
+	t.plan(1)
+	t.pass('Test passed')
+})
